761002b2474b29d425e64da2faf044fd5e8deda3
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2022-11-15 15:55:09 -0300

    começo do sistema de login/cadastro

--- milestone_2/src/components/Cadastro.jsx
+++ milestone_2/src/components/Cadastro.jsx
@@ -1,42 +1,77 @@
 import React from 'react';
+import Cookies from 'universal-cookie';
 import "./Cadastro.css"
+
+function delay(){
+  return new Promise(function(resolve) {
+      setTimeout(resolve, 100);
+  });
+}
+
+
+
 const Cadastro = () => {
+    const cookies = new Cookies();
+    const handleCadastro = async () => {
+        //primeiro checar se as senhas coincidem
+        let campoSenha = document.getElementById("campo-senha").value;
+        let campoConfirmarSenha = document.getElementById("campo-confirmar-senha").value;
+        if(campoConfirmarSenha !== campoSenha){
+            window.alert("Senhas não coincidem")
+            return;
+        }
+       
+
+        let campoEmail = document.getElementById("campo-email").value;
+        let campoUsername = document.getElementById("campo-username").value;
+        let new_user = {
+            email : campoEmail,
+            username : campoUsername,
+            senha : campoSenha
+        };
+        await delay();
+        cookies.set(new_user.email, new_user);
+        return;
+    }
+
+
+
     return ( 
         <>
         <div className="cadastro-form">
                 <div className="nome">
                     <p>Nome</p>
-                    <input type="text" name="nome" id="" className='campo'/>
+                    <input type="text" name="nome" id="campo-nome" className='campo'/>
                 </div>
                 <div className="username">
                     <p>Username</p>
-                    <input type="text" name="username" id="" className='campo'/>
+                    <input type="text" name="username" id="campo-username" className='campo'/>
                 </div>
                 <div className="email">
                     <p>Email</p>
-                    <input type="email" name="email" id="" className='campo'/>
+                    <input type="email" name="email" id="campo-email" className='campo'/>
                 </div>
                 <div className="telefone">
                     <p>Telefone</p>
-                    <input type="text" name="telefone" id="" className='campo'/>
+                    <input type="text" name="telefone" id="campo-telefone" className='campo'/>
                 </div>
                 <div className="cep">
                     <p>Cep</p>
-                    <input type="text" name="cep" id="" className='campo'/>
+                    <input type="text" name="cep" id="campo-cep" className='campo'/>
                 </div>
                 <div className="logradouro">
                     <p>Logradouro</p>
-                    <input type="text" name="logradouro" id="" className='campo'/>
+                    <input type="text" name="logradouro" id="campo-logradouro" className='campo'/>
                 </div>
                 <div className="senha">
                     <p>Senha</p>
-                    <input type="password" name="senha" id="" className='campo'/>
+                    <input type="password" name="senha" id="campo-senha" className='campo'/>
                 </div>
                 <div className="confirmar-senha">
                     <p>Confirmar Senha</p>
-                    <input type="password" name="senha" id="" className='campo'/>
+                    <input type="password" name="senha" id="campo-confirmar-senha" className='campo'/>
                 </div>
-                <button className='cadastro-button'>Cadastro</button>
+                <button onClick={handleCadastro} className='cadastro-button'>Cadastro</button>
         </div>
 
         </>
--- milestone_2/src/components/Login.jsx
+++ milestone_2/src/components/Login.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Login.css"
+import Cookies from 'universal-cookie';
+function delay(){
+  return new Promise(function(resolve) {
+      setTimeout(resolve, 100);
+  });
+}
 const Login = () => {
+    
+    const handleLogin = async () =>{
+        let cookies = new Cookies();
+        let email = document.getElementById("campo-email").value;
+        let senha = document.getElementById("campo-senha").value;
+        await delay();
+        let retorno = cookies.get(email);
+        if(retorno === undefined || senha !== retorno.senha){
+            window.alert("email ou senha incorretos");
+            return;
+
+        }
+
+        cookies.set("logged_user", retorno);
+        window.alert("logado");
+
+    }
     return (
         <>
         <div className="login-form">
                 <div className="email">
                     <p>Email</p>
-                    <input type="email" name="email" id="" className='campo'/>
+                    <input type="email" name="email" id="campo-email" className='campo'/>
                 </div>
                 <div className="senha">
                     <p>Senha</p>
-                    <input type="password" name="senha" id="" className='campo'/>
+                    <input type="password" name="senha" id="campo-senha" className='campo'/>
                 </div>
-                <button className='login-button'>Login</button>
+                <button onClick={handleLogin} className='login-button'>Login</button>
         </div>
         <div className='sem-conta'>
             <p>Não tem uma conta?</p>
