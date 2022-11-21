39df1b35237845cd19a9b85066aeac626c8b7c69
Rafael de Almeida <rafaeldealmeida@usp.br>
2022-11-20 22:28:54 -0300

    carrinho finalizado, adiconar ao carrinho funcionando

--- milestone_2/src/components/Cart.jsx
+++ milestone_2/src/components/Cart.jsx
@@ -4,39 +4,94 @@ import Card from './Card';
 import "./Cart.css";
 import smw from './smw.png';
 
-//o cart é um array de objeto game e quantidade
-let cart = [{
-    game: {
-        name: 'Super Mario World',
-        price: 'R$ 56,90',
-        img: smw,
-        console: 'SNES'
-    },
-    quantidade: 2
-},
-{
-    game: {
-        name: 'Super Mario World',
-        price: 'R$ 56,90',
-        img: smw,
-        console: 'SNES'
-    },
-    quantidade: 2
-},{
-    game: {
-        name: 'Super Mario World',
-        price: 'R$ 56,90',
-        img: smw,
-        console: 'SNES'
-    },
-    quantidade: 2
-}]
+let cart = []
 
-const remove = (event) => {
-    event.currentTarget.parentNode.remove()
+//o cart é um array de objeto game e quantidade
+// let cart = [{
+//     game: {
+//         name: 'Super Mario World',
+//         price: '56.90',
+//         img: smw,
+//         console: 'SNES'
+//     },
+//     quantidade: 1
+// },
+// {
+//     game: {
+//         name: 'Super Mario World',
+//         price: '56.90',
+//         img: smw,
+//         console: 'SNES'
+//     },
+//     quantidade: 1
+// },
+// {
+//     game: {
+//         name: 'Super Mario World',
+//         price: '56.90',
+//         img: smw,
+//         console: 'SNES'
+//     },
+//     quantidade: 1
+// }]
+
+const refresh = () => {
+    let campos = document.getElementsByClassName('value-2')
+
+    let totalPrice = 0
+    let totalItens = 0
+
+    cart.forEach(function(element, i){
+        totalPrice += parseFloat((element.game.price).replace(',','.')) * parseInt(element.quantidade)
+        totalItens += parseInt(element.quantidade)
+    })
+
+    campos[0].innerHTML = totalItens.toString()
+    campos[2].innerHTML = "R$ " + totalPrice.toFixed(2).toString()
+    campos[4].innerHTML = "5x de R$ " + (totalPrice/5).toFixed(2).toString()
 }
 
-const Cart= ({/*cart*/}) => {
+const remove = (event) => {
+
+    let removeName = event.currentTarget.nextSibling.children[3].innerHTML
+
+    for(let i = 0; i < cart.length; i++){
+        if(cart[i].game.name == removeName){
+            cart.splice(i,1)
+            break;
+        }
+    }
+
+    window.localStorage.setItem("cart",JSON.stringify(cart));
+
+    event.currentTarget.parentNode.remove()
+    refresh()
+}
+
+const endCart = () => {
+    alert("Compra finalizada :)")
+
+    document.getElementsByClassName('informations-left')[0].innerHTML = ""
+
+    cart = []
+    window.localStorage.setItem("cart",JSON.stringify(cart));
+
+    refresh()
+}
+
+const Cart= () => {
+
+    cart = localStorage.getItem("cart")
+    cart = JSON.parse(cart)
+
+    let totalPrice = 0
+    let totalItens = 0
+
+    cart.forEach(function(element, i){
+        totalPrice += parseFloat((element.game.price).replace(',','.')) * parseInt(element.quantidade)
+        totalItens += parseInt(element.quantidade)
+    })
+
     return (
         <div className='cart-page'>
             <div className='informations'>
@@ -57,15 +112,15 @@ const Cart= ({/*cart*/}) => {
                 <div className='informations-right'>
                     <div className='row'>
                         <p className='value-1'>Itens: </p>
-                        <p className='value-2'>2</p>
+                        <p className='value-2'>{totalItens}</p>
                     </div>
                     <div className='row'>
                         <p className='value-1'>Frete: </p>
-                        <p className='value-2'>R$ 27,87</p>
+                        <p className='value-2'>R$ 00,00</p>
                     </div>
                     <div className='row'>
                         <p className='value-1'>Total: </p>
-                        <p className='value-2'>R$ 141,66</p>
+                        <p className='value-2'>R$ {totalPrice.toFixed(2)}</p>
                     </div>
                     <div className='row'>
                         <p className='value-1'>Pagamento: </p>
@@ -73,14 +128,14 @@ const Cart= ({/*cart*/}) => {
                     </div>
                     <div className='row'>
                         <p className='value-1'>Parcelamento: </p>
-                        <p className='value-2'>3x de R$ 47,22</p>
+                        <p className='value-2'>5x de R$ {(totalPrice/5).toFixed(2)}</p>
                     </div>
                     <div className='row'>
                         <p className='value-1'>Juros: </p>
                         <p className='value-2'>R$ 00,00</p>
                     </div>
-                    <button className='edit-button'>
-                        <Link className='button' to="/">FINALIZAR COMPRA</Link>  {/*Não é rota, só botar um bglh ai pra alterar a quantidade*/}
+                    <button onClick={endCart} className='edit-button'>
+                        FINALIZAR COMPRA
                     </button>
                 </div>
             </div>
--- milestone_2/src/components/Product.jsx
+++ milestone_2/src/components/Product.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Card from './Card';
 import "./Product.css";
 import smw from './smw.png';
@@ -7,13 +6,47 @@ import smw from './smw.png';
 //objeto de game de exemplo
 let game = {
     name: 'Super Mario World',
-    price: 'R$ 56,90',
+    price: '56,90',
     img: smw,
     console: 'SNES',
     description: 'Jogo usado em perfeito estado. Versão dos Estados Unidos'
 }
 
-const Product = ({/*game*/}) => {
+const buyProduct = () => {
+    alert("O produto foi comprado :)")
+}
+
+const addToCart = () => {
+
+    let cart = localStorage.getItem("cart")
+    cart = JSON.parse(cart)
+
+    let isOnCart = false
+    
+    cart.forEach(function(element,i){
+        if(element.game.name === game.name){
+            isOnCart = true
+            element.quantidade = parseInt(element.quantidade) + 1
+        }
+    })
+
+    if(!isOnCart){
+        let cartItem = {
+            game,
+            quantidade: 1
+        }
+
+        cart.push(cartItem)
+    }
+
+    console.log(cart)
+
+    localStorage.setItem("cart",JSON.stringify(cart))
+
+    alert("Produto adicionado ao carrinho")
+}
+
+const Product = () => {
     return (
         <div className='product-page'>
             <div className='informations'>
@@ -22,24 +55,21 @@ const Product = ({/*game*/}) => {
                 </div>
                 <div className='informations-right'>
                     <div className='row-1'>
-                        <p>{game.name} <br/> [{game.console}]</p>
+                        <p>{game.name}</p>
+                        <p>[{game.console}]</p>
                     </div>
                     <div className='row-2'>
                         <div className='values'>
                             <div className='value-1'>
-                                <p>{game.price}</p>
+                                <p>R$ {game.price}</p>
                             </div>
                             <div className='value-2'>
                                 <p>5x de R$ 11,38</p>
                             </div>
                         </div>
                         <div className='all-buttons'>
-                            <button className='edit-button'>
-                                <Link className='button' to="/admin/products">Comprar</Link>
-                            </button>
-                            <button className='edit-button'>
-                                <Link className='button' to="/admin/products">Carrinho</Link>
-                            </button>
+                            <button onClick={buyProduct} className='edit-button'> Comprar </button>
+                            <button onClick={addToCart} className='edit-button'> Carrinho </button>
                         </div>
                     </div>
                     <div className='row-3'>
