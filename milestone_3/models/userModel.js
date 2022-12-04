7f3573a012dec8eb9fa4122e3d5ba316f53c40c3
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2022-12-04 17:06:01 -0300

    adicionado verficação de existentes no crud do user

--- milestone_3/models/userModel.js
+++ milestone_3/models/userModel.js
@@ -1,14 +1,29 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-    nome: String,
-    email: String,
-    username: String,
+    nome:{
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true
+        },
+    username:{
+        type: String,
+        unique: true,
+        trim: true},
     senha: String,
-    endereco: String,
-    telefone: String,
+    endereco: {
+        type: String,
+        trim: true
+    },
+    telefone: {
+        type: String,
+        trim: true
+    },
     adm : Boolean
 
 })
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema)
