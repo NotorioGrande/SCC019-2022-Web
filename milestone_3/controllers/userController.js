0c1dff9e5a9df933a0d60f55042e14a60b591f78
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2022-12-04 16:03:13 -0300

    crud de user feito sem verificacoes acerca se o username ou email ja existe

--- milestone_3/controllers/userController.js
+++ milestone_3/controllers/userController.js
@@ -15,3 +15,62 @@ module.exports.cadastrarUser= async (req, res) => {
     const userCreated = await newUser.save();
     return res.status(200).json(userCreated);
 }
+module.exports.getAllUsers = async (req, res) => {
+    const usersFound = await userModel.find();
+    
+    if(usersFound.length > 0)
+        return res.status(200).json(usersFound);
+    
+    return res.status(404).send("Sem usuários no sistema");
+}
+
+module.exports.getUser = async (req, res) =>{
+    const id = req.params.id;
+    if(!mongoose.isValidObjectId(id)){
+        return res.status(404).send("Nenhum usuário com esse ID")
+    }
+    const userFound = await userModel.find({_id : id});
+    if(userFound){
+        return res.status(200).json(userFound);
+    }
+    return res.status(404).send("Nenhum usuário com esse ID")
+    
+
+
+}
+module.exports.deleteUser = async (req, res) =>{
+    const id = req.params.id;
+    if(!mongoose.isValidObjectId(id)){
+        return res.status(404).send("Nenhum usuário com esse ID")
+    }
+    const userFound = await userModel.findOneAndDelete({_id : id});
+    if(userFound){
+        return res.status(200).json(userFound);
+    }
+    return res.status(404).send("Nenhum usuário com esse ID")
+    
+}
+
+module.exports.updateUser = async (req, res) => {
+    const id = req.params.id;
+    if(!mongoose.isValidObjectId(id)){
+        return res.status(404).send("Nenhum usuário com esse ID")
+    }
+    const updatedUser = await userModel.findOneAndUpdate({_id : id}, {
+        nome: req.body.nome,
+        email: req.body.email,
+        username: req.body.username,
+        senha: req.body.senha,
+        endereco: req.body.endereco,
+        telefone: req.body.telefone,
+        adm :req.body.adm 
+    }, {
+        new: true
+    })
+    if(updatedUser) {
+        return res.status(200).json(updatedUser);
+    }
+    return res.status(404).send("Nenhum usuário com esse ID");
+    
+
+}
--- milestone_3/routes/user.js
+++ milestone_3/routes/user.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const userController = require("../controllers/userController.js");
 
 router.post("/api/user", userController.cadastrarUser);
+router.get("/api/user", userController.getAllUsers);
+router.get("/api/user/:id", userController.getUser);
+router.delete("/api/user/:id", userController.deleteUser);
+router.put("/api/user/:id", userController.updateUser);
 
 module.exports = router;
