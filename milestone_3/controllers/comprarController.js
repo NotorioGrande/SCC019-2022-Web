e9812c701066f39167d5647b537a2eea0c0bbcc0
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2022-12-10 14:44:20 -0300

    processar carrinho pronto

--- milestone_3/controllers/comprarController.js
+++ milestone_3/controllers/comprarController.js
@@ -14,3 +14,23 @@ module.exports.lidarCompra = async (req, res) => {
     }
     return res.status(200).json(result);
 }
+
+module.exports.processarCarrinho = async (req, res) => {
+    let produtos = req.body;
+    let retorno;
+    for(let produto of produtos){
+        const idProduto = produto.idProduto;
+        const idUsuario = produto.idUsuario;
+        const quantidade = produto.quantidade;
+        try{
+            retorno = await comprasHelper.comprar(idProduto, quantidade, idUsuario);
+        }
+        catch(e){
+            console.log(e.message)
+            let errorObj = {error : e.message}
+            return res.status(404).json(errorObj);
+        }
+    }
+    return res.status(200).json(retorno);
+
+}
--- milestone_3/routes/comprar.js
+++ milestone_3/routes/comprar.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const router = express.Router();
 const compraControler = require('../controllers/comprarController');
-router.post("/api/comprar", compraControler.lidarCompra)
+router.post("/api/comprar", compraControler.lidarCompra);
+router.post("/api/carrinho", compraControler.processarCarrinho);
 
 module.exports = router;
